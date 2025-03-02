7025caefa5cfa58a01ca024fc18d09c1b7f67c5e
yunbang-app-name <45517795+yunbang-app@users.noreply.github.com>
2025-03-02 22:44:15 +0800

    Update TheWelcome.vue 567

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -14,7 +14,7 @@ const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
     <template #icon>
       <DocumentationIcon />
     </template>
-    <template #heading>Documentation 456</template>
+    <template #heading>Documentation 567</template>
 
     Vue’s
     <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
